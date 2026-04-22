28d3e77038e7416533e75dbeccd5b9e4d45ee083
Aisha Aijaz Ahmad <aishaaijazahmad@gmail.com>
2026-04-22 23:52:26 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -8,3 +8,24 @@ toggle.addEventListener("click", () => {
         document.body.setAttribute("data-theme", "dark");
     }
 });
+
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 100) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
