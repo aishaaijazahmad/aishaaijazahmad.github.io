f56829646051c547e12b4209799275e0845635f9
Aisha Aijaz Ahmad <aishaaijazahmad@gmail.com>
2026-04-23 16:02:46 +0530

    Update theme.js

--- js/theme.js
+++ js/theme.js
@@ -6,19 +6,18 @@ const savedTheme = localStorage.getItem("theme");
 
 if (savedTheme) {
     document.documentElement.setAttribute("data-theme", savedTheme);
-    icon.textContent = savedTheme === "dark" ? "☀" : "☾";
+    icon.src = savedTheme === "dark" ? "assets/sun.svg" : "assets/moon.svg";
 } else {
-    icon.textContent = "☾"; // default
+    icon.src = "assets/moon.svg";
 }
 
-// Toggle theme
+// Toggle
 toggle.addEventListener("click", () => {
     const current = document.documentElement.getAttribute("data-theme");
-
     const newTheme = current === "dark" ? "light" : "dark";
 
     document.documentElement.setAttribute("data-theme", newTheme);
     localStorage.setItem("theme", newTheme);
 
-    icon.textContent = newTheme === "dark" ? "☀" : "☾";
+    icon.src = newTheme === "dark" ? "assets/sun.svg" : "assets/moon.svg";
 });
